d9bc84a234844054c2f5ab4fb9bfc64dfc3266e5
yuki <yuki>
2016-07-17 15:32:04 +0900

    first commi

---
+++ WpfApplication1/DB/DBmaneg.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using System.Data;
+
+namespace WpfApplication1.DB
+{
+    class DBmaneg
+    {
+        public void repository()
+        {
+            //データベースに接続するための処理（情報）
+            var connString = @"Server=localhost;Port=5431;User Id=postgres;Password#;Database=student";
+            //DataReaderを利用したＳＥＬＥＣＴ
+            using (var conn = new NpgsqlConnection(connString))
+            {
+                conn.Open();
+                var command = new NpgsqlCommand(@"select * from table_name", conn);
+
+                var dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    Console.WriteLine("value : {0},", dataReader["column_name"]);
+                }
+            }
+                using (var conn =new NpgsqlConnection(connString))
+                {
+                    conn.Open();
+
+                var dataAdapter = new NpgsqlDataAdapter(@"SELECT * table_name",conn);
+
+                var dataSet = new DataSet();
+                dataAdapter.Fill(dataSet);
+
+                Console.WriteLine(dataSet.Tables[0].Rows[0]["column_name"]);
+
+                }
+            }
+        }
+
+         
+
+    }
+
